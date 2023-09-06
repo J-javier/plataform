3400b57a8c949db126b946791883153c34e50d9c
unknown <jose360pelico@gmail.com>
2023-09-06 00:51:25 -0600

    Data base was created

---
+++ src/database/connection.php
@@ -0,0 +1,50 @@
+<?php
+
+$file = $_SERVER["DOCUMENT_ROOT"] . "/.env";
+
+if (file_exists($file)){
+    $variables = parse_ini_file($file);
+    $_ENV = $variables;
+}
+
+
+class data 
+{
+
+    private $pdo;
+
+    public function __construct()
+        
+    {
+        try {
+            $host = $_ENV["DB_HOST"];
+            $username = $_ENV["DB_USERNAME"];
+            $password = $_ENV["DB_PASSWORD"];
+            $port = $_ENV["DB_PORT"];
+            $dbname = $_ENV["DB_DATA"];
+
+            $attributes=[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+            $dsn = "mysql:host=$host;port=$port;dbname=$dbname ";
+
+            $this->pdo = new PDO($dsn,$username,$password,$attributes);
+
+        } catch (PDOException $e) {
+            echo "ERROR: " . $e->getMessage();
+        }
+
+    }
+
+    public function connect()
+    {
+        return $this->pdo;
+    }
+
+
+}
+
+
+
+
+
+
+?> 
