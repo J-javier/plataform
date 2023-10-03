21a9604a29204069f4f50d7c7dc1366d44acfc21
unknown <jose360pelico@gmail.com>
2023-10-03 13:46:34 -0600

    database was updated

--- src/database/plataform.sql
+++ src/database/plataform.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-10-2023 a las 19:51:43
+-- Tiempo de generación: 03-10-2023 a las 21:45:14
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,46 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `curso`
+-- Estructura de tabla para la tabla `maestro`
 --
 
-CREATE TABLE `curso` (
-  `id_curso` int(11) NOT NULL,
-  `name_curso` varchar(200) DEFAULT NULL
+CREATE TABLE `maestro` (
+  `id_maestro` int(11) NOT NULL,
+  `name_maestro` varchar(150) DEFAULT NULL,
+  `apellido_maestro` varchar(150) DEFAULT NULL,
+  `matricula` varchar(100) DEFAULT NULL,
+  `correo_maestro` varchar(200) DEFAULT NULL,
+  `password_maestro` varchar(250) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `fechaNacimiento` varchar(80) DEFAULT NULL,
+  `rol_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `curso`
+-- Volcado de datos para la tabla `maestro`
 --
 
-INSERT INTO `curso` (`id_curso`, `name_curso`) VALUES
-(1, 'Matemáticas'),
-(2, 'Historia'),
-(3, 'Ciencias'),
-(4, 'Inglés'),
-(5, 'Inglés'),
-(6, 'Programación'),
-(7, 'Portugues');
+INSERT INTO `maestro` (`id_maestro`, `name_maestro`, `apellido_maestro`, `matricula`, `correo_maestro`, `password_maestro`, `address`, `fechaNacimiento`, `rol_id`) VALUES
+(6, 'Jira', '', 'H098s8', 'jirafales@mail.com', '$2y$10$X3YO5rro2ODvgqALHXHZ6OtpXvyA6YU0hzBuA9lE6BLWxyF.F9Mle', '', '', 3),
+(7, 'Pedro', 'Aquiles', 'J8d8765', 'PedroA@mail.com', '$2y$10$1OHw3ekyZDk3.JGiuUmyBOTb02JVYWVH05v0QN6bcojEVRkS99uj6', 'Fresno 98', '1990-11-24', 3),
+(8, 'maestro', 'prueba', 'F232445', 'maestro@maestro.com', '$2y$10$WNzvyRzcbh7cDdCtyFIrse19yHL616LeqXn5iMCKb7iN.G3hUwnC6', '98 Calle ', '1989-02-23', 3);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `curso`
+-- Indices de la tabla `maestro`
 --
-ALTER TABLE `curso`
-  ADD PRIMARY KEY (`id_curso`);
+ALTER TABLE `maestro`
+  ADD PRIMARY KEY (`id_maestro`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `curso`
+-- AUTO_INCREMENT de la tabla `maestro`
 --
-ALTER TABLE `curso`
-  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `maestro`
+  MODIFY `id_maestro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
